e27771148ee64cabffc4a506c62df36fb418da2a
Tim Jarratt <tjarratt@gmail.com>
2014-02-08 12:26:04 -0800

    Improve usage

--- dump_ast.go
+++ dump_ast.go
@@ -10,7 +10,7 @@ import(
 
 func main() {
 	if len(os.Args) != 2 {
-		println("expected exactly one arg: path to a file to inspect")
+		println("usage: dump_ast  path/to/a/file.go")
 		os.Exit(1)
 	}
 

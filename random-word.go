4316b9fcaaf8c6d0aa7607d1dd9ed00a851c6fd0
Tim Jarratt <tjarratt@gmail.com>
2014-02-17 19:00:19 -0800

    Read in /usr/share/dict/words
    Fails horribly on windows, but that's a task for tomorrow. Also,
    returns the same random int each time.

--- random-word.go
+++ random-word.go
@@ -3,19 +3,62 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"math/rand"
 	"strings"
 )
 
-separator := "-"
-numberOfWords := 3
+var (
+	separator string
+	numberOfWords int
+)
+
 func init() {
-	flag.IntVar("n", &numberOfWords)
-	flag.StringVar("s", &separator)
+	flag.IntVar(&numberOfWords, "n", 3, "the number of random words to join")
+	flag.StringVar(&separator, "s", "-", "a separator to use when joining words")
 }
 
+// TODO: break the random word functionality into windows && unix helpers
 func main() {
+	if len(os.Args) > 1 {
+		checkUsage()
+	}
+
+	words, err := readAvailableDictionary()
+	if err != nil {
+		println("Sorry, some unexpected happening reading your dictionary:")
+		println(err.Error())
+		os.Exit(2)
+	}
+
+	flag.Parse()
+	pieces := []string{}
+	for i := 0; i < numberOfWords; i++ { // FIXME: rand should be seeded
+		pieces = append(pieces, words[rand.Int() % len(words)])
+	}
+
+	println(strings.Join(pieces, separator))
+	return
+}
+
+// this will fail horribly on windows
+func readAvailableDictionary() (words []string, err error) {
+	file, err := os.Open("/usr/share/dict/words")
+	if err != nil {
+		return
+	}
+
+	bytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		return
+	}
+
+	words = strings.Split(string(bytes), "\n")
+	return
+}
+
+func checkUsage() {
 	if os.Args[1] == "help" || os.Args[1] == "-h" || os.Args[1] == "--help" {
 		fmt.Printf(`
 usage: random-word -s [separator] -n [number-of-words]
@@ -26,14 +69,4 @@ The number of words printed defaults to 3
 `)
 		os.Exit(1)
 	}
-
-	flag.Parse()
-	pieces := []string{}
-	words = []string{"holy", "moly", "guacamole", "oily", "strombole"}
-	for i := 0; i < numberOfWords {
-		pieces = append(pieces, words[rand.Int() % len(words)])
-	}
-
-	println(strings.Join(pieces, separator))
-	return
 }
